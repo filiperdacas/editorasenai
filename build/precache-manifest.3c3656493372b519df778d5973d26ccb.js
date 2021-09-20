0fda30674d2b36be3935f4510ecf7b98737adf34
filiperdacas <filipe.cas@edu.sc.senai.br>
2021-09-20 20:32:26 -0300

    comit certo

---
+++ build/precache-manifest.3c3656493372b519df778d5973d26ccb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d6664d2648ebf65339f1d87ee05d5ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "caba204062b9f4914aa2",
+    "url": "/static/css/main.618940e1.chunk.css"
+  },
+  {
+    "revision": "7f7b9347409b383f9e6e",
+    "url": "/static/js/2.0c83bead.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0c83bead.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "caba204062b9f4914aa2",
+    "url": "/static/js/main.7ee02449.chunk.js"
+  },
+  {
+    "revision": "e9419607096510474d1a",
+    "url": "/static/js/runtime-main.81c04eb7.js"
+  }
+]);
